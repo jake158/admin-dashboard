dacc5849918ddbc86bfc64b02c960be11f03ed90
freeram <kulikovpavel2801@gmail.com>
2024-06-04 17:56:29 -0600

    Make project buttons clickable

--- javascript.js
+++ javascript.js
@@ -1,2 +1,24 @@
 'use strict';
 
+const projectCards = document.querySelectorAll('.project-card');
+
+projectCards.forEach(card => {
+    const favoriteButtonIcon = card.querySelector('button img[alt="favorite button icon"]');
+    const watchButtonIcon = card.querySelector('button img[alt="watch button icon"]');
+
+    favoriteButtonIcon.parentElement.addEventListener('click', function () {
+        if (favoriteButtonIcon.src.includes('star-outline.svg')) {
+            favoriteButtonIcon.src = 'icons/star.svg';
+        } else {
+            favoriteButtonIcon.src = 'icons/star-outline.svg';
+        }
+    });
+
+    watchButtonIcon.parentElement.addEventListener('click', function () {
+        if (watchButtonIcon.src.includes('eye-outline.svg')) {
+            watchButtonIcon.src = 'icons/eye.svg';
+        } else {
+            watchButtonIcon.src = 'icons/eye-outline.svg';
+        }
+    });
+});
